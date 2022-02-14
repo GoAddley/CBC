b8a87ef94f18ff48f4e74668a3ff1c648fd29f86
GoAddley <ashl3y_n@hotmail.com>
2022-02-14 16:21:32 +1000

    Finalised screen resolutions for other screens

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('mouseenter', () => {
-    btn.style.width = '40%'
-    btn.style.height = '60px';
-    btn.style.lineHeight = '45px';
-    btn.style.transition = '0.4s';
-});
-
-btn.addEventListener('mouseleave', () => {
-    btn.style.width = '35%';
-    btn.style.height = '50px';
-    btn.style.lineHeight = '35px';
-});
