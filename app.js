75af32351d9e93b27488806d85d369aa36db0df3
GoAddley <ashl3y_n@hotmail.com>
2022-02-17 13:43:44 +1000

    Added JavaScript to page and added some finishing touches after

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-const btn = document.querySelector('.btn');
+const card1 = document.querySelectorAll(".counselling-card");
+const card2 = document.querySelectorAll(".counselling-card2");
+const fees = document.querySelectorAll(".price-card");
 
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 1750 || document.documentElement.scrollTop > 1750) {  
+    card1.forEach(card => {
+        card.style.right = '0%';
+        card.style.opacity = '1'
+        card.style.transition = 'all 1s';
+    });
+    card2.forEach(card => {
+        card.style.left = '0%';
+        card.style.opacity = '1'
+        card.style.transition = 'all 1s';
+    });
+  } if (document.body.scrollTop > 3600 || document.documentElement.scrollTop > 3600) {
+    fees.forEach(fee => {
+        fee.style.opacity = '1';
+        fee.style.transition = 'all 2s';
+    });
+    }
+}
